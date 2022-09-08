fe2969b9643ddc894978193797bc029f93ff79ca
Kuan-Hsun Chen <k.h.chen@utwente.nl>
2022-09-08 14:40:13 +0200

    Create ploters

--- plots/plot.sh
+++ plots/plot.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-#python plot.py -i rtss -u 45 -s 20 -f 0.025 -h 1.83 -v prob_log 
-#python plot.py -i rtss -u 60 -s 100 -f 0.025 -h 1.83 -v prob_log
-#python plot.py -i rtss -u 80 -s 100 -f 0.025 -h 1.83 -v prob_log
-python plot.py -i rtss -u 45 -s 10 -f 0.025 -h 1.83 -v prob_log
+# Figure 6
+python plot6.py -i rtss -n 5 -u 60 -s 10 -f 0.025 -h 1.83 -l
+python plot6.py -i rtss -n 5 -u 80 -s 10 -f 0.025 -h 1.83 -l
+# Figure 7
+
+python plot7.py -i rtss -n 5 -u 60 -s 10 -f 0.025 -h 1.83 
+python plot7.py -i rtss -n 5 -u 80 -s 10 -f 0.025 -h 1.83 
+
+# Figure 8
+python plot8.py -i rtss -n 5 -u 60 -s 10 -f 0.025 -h 1.83 
+
+# Figure 9
+python plot9.py -i rtss -n 2 -u 45 -s 10 -f 0.025 -h 1.83 -l
+python plot9.py -i rtss -n 5 -u 45 -s 10 -f 0.025 -h 1.83 -l
+
+# Figure 10
+python plot10.py -i rtss -n 2 -u 45 -s 10 -f 0.025 -h 1.83 
+python plot10.py -i rtss -n 5 -u 45 -s 10 -f 0.025 -h 1.83 
+
+# Figure 11
+python plot11.py -i rtss -n 15 -u 45 -s 10 -f 0.025 -h 1.83 
+python plot11.py -i rtss -n 25 -u 45 -s 10 -f 0.025 -h 1.83 
 
 sleep 1
 
--- plots/plot6.py
+++ plots/plot6.py
@@ -22,9 +22,7 @@ rcParams['font.sans-serif'] = ['Tahoma']
 rcParams['ps.useafm'] = True
 rcParams['pdf.use14corefonts'] = True
 rcParams['text.usetex'] = True
-rcParams["figure.figsize"] = (8,9) # for 6 sets - Figure 8 
-#rcParams["figure.figsize"] = (5,9) # for 3 sets - Figure 6-7
-#rcParams["figure.figsize"] = (5,9) # for 2 sets - Figure 9-11
+rcParams["figure.figsize"] = (5,9) # for 3 sets - Figure 6-7
 
 def plot_datasets(dataset, utilization):
     # format the input as the result
@@ -40,38 +38,23 @@ def plot_datasets(dataset, utilization):
         conv_ori.append(inputs[0])
         conv_carry.append(inputs[1])
         conv_inflation.append(inputs[2])
-        ori.append(inputs[3])
-        carry.append(inputs[4])
-        inflation.append(inputs[5])
+        #ori.append(inputs[3])
+        #carry.append(inputs[4])
+        #inflation.append(inputs[5])
     
     # Figure 6-7
-    #bxinput.append(conv_ori)
-    #bxinput_2.append(conv_carry)
-    #bxinput_2.append(conv_inflation)
-
-    # Figure 8
     bxinput.append(conv_ori)
-    bxinput.append(ori)
-    
     bxinput_2.append(conv_carry)
     bxinput_2.append(conv_inflation)
-    bxinput_2.append(carry)
-    bxinput_2.append(inflation)
-    
-    #bxinput.append(ori)
-    #bxinput.append(carry)
-    #bxinput.append(inflation)
 
     figlabel = itertools.cycle(('a','b','c','d','e','f','g','h','i'))
     marker = itertools.cycle(('o', 'v','*','D','x','+'))
     colors = itertools.cycle(('c','r','b','g','r','y','y','b'))
     names = []
     fig = plt.figure()    
-    gs = gridspec.GridSpec(1, 2, width_ratios=[1.1, 2]) 
-    #ax = fig.add_subplot(111)
+    gs = gridspec.GridSpec(1, 2, width_ratios=[1.1, 2])     
     ax = plt.subplot(gs[0])
     ax.set_facecolor('0.9')
-    #fig.subplots_adjust (top = 0.5, bottom = 0.2, left = 0.1, right = 0.95, hspace = 0.3, wspace=0.05)
     fig.subplots_adjust (top = 0.5, bottom = 0.2, left = 0.1, right = 0.95, hspace = 0.3, wspace=0.05)
     ax.set_yscale("log")
     ax.set_ylabel('WCDFP (log-scale)',size=20)
@@ -79,35 +62,15 @@ def plot_datasets(dataset, utilization):
     ax.vlines(0.5, 0, 1, linestyles='dotted', transform=ax.transAxes)
 
     # Figure 6 
-    # U80
-    #ax.set_ylim([10**-8, 10**0.5])
-    # U60
-    #ax.set_ylim([10**-18, 10**1])
-    #ax.set_yticks([10**-18, 10**-15, 10**-12, 10**-9, 10**-6, 10**-3, 10**0]) # for Conv sets U60/80
-
-    # Figure 7
-    # U80
-    #ax.set_ylim([10**-18, 10**1])
-    # U60
-    #ax.set_ylim([10**-63, 10**4])
-    #ax.set_yticks([10**-60, 10**-48, 10**-36, 10**-24, 10**-12, 10**0]) # for Conv sets U60/80
-   
-    # Figure 8
-    ax.set_ylim([10**-84, 10**5])
-
-    # Figure 10
-    #ax.set_ylim([10**-30, 10**5])
-    #ax.set_yticks([10**-30, 10**-20, 10**-10, 10**0]) # for CB sets U45
-    # Figure 11
-    #ax.set_ylim([10**-300, 10**5]) # for CB sets
-    #ax.set_yticks([10**-300, 10**-200, 10**-100, 10**0]) # for CB sets task 25/15
-
-       
-    #labels = ['Conv-CarryIn','Conv-Inflation', 'CB-CarryIn', 'CB-Inflation'] # 4 sets
-    #labels = ['Conv-Refuted','Conv-CarryIn','Conv-Inflation'] # 3 sets
-    #labels = ['Conv-Refuted'+r'$\times$'] # 1 set
-    labels = ['Conv-Refuted'+r'$\times$', 'CB-Refuted'+r'$\times$'] # 2 set
-    #labels = ['CB-Refuted','CB-CarryIn','CB-Inflation'] # 3 sets
+    if utilization == 80:
+        # U80
+        ax.set_ylim([10**-8, 10**0.5])
+    else:
+        # U60
+        ax.set_ylim([10**-18, 10**1])
+        ax.set_yticks([10**-18, 10**-15, 10**-12, 10**-9, 10**-6, 10**-3, 10**0]) # for Conv sets U60/80    
+           
+    labels = ['Conv-Refuted'+r'$\times$'] # 1 set    
     #the blue box
     boxprops = dict(linewidth=2, color='blue')
     #the median line
@@ -119,47 +82,30 @@ def plot_datasets(dataset, utilization):
         tick.label.set_fontsize(14)
 
     for tick in ax.yaxis.get_major_ticks():
-        tick.label.set_fontsize(18)
-    # Figure 8
-    ax.boxplot(bxinput, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6, 0.6)) # 2 sets
-    #ax.boxplot(bxinput, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6, 0.6, 0.6, 0.6, 0.6, 0.6)) # 6 sets
-    #ax.boxplot(bxinput, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6, 0.6, 0.6)) # 3 sets
-    
-    # Figure 6-7
-    #ax.boxplot(bxinput, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6))
+        tick.label.set_fontsize(18)        
+    ax.boxplot(bxinput, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6))
+    ax.grid()
 
-    # Figure 5-7 -- second group
+    # Figure 6-7 -- second group
     ax = plt.subplot(gs[1])
     fig.subplots_adjust (top = 0.5, bottom = 0.2, left = 0.1, right = 0.95, hspace = 0.3, wspace=0.05)
     
-    # Figure 5 U80
-    #ax.set_ylim([10**-8, 10**0.5])
-    # Figure 5 U60
-    #ax.set_ylim([10**-18, 10**1])
-
-    # Figure 6
-    # U80
-    #ax.set_ylim([10**-18, 10**1])
-    # U60
-    #ax.set_ylim([10**-63, 10**4])
-    #ax.set_yticks([10**-60, 10**-48, 10**-36, 10**-24, 10**-12, 10**0]) # for Conv sets U60/80
-    
-    # Figure 8
-    ax.set_ylim([10**-84, 10**5])
-    #ax.set_yticks([10**-100, 10**-75, 10**-50, 10**-25, 10**0]) 
-    labels = ['Conv-CarryIn','Conv-Inflation', 'CB-CarryIn', 'CB-Inflation'] # 4 sets
-    ax.vlines(0.5, 0, 1, linestyles='dotted', transform=ax.transAxes)
+    # Figure 6 
+    if utilization == 80:
+        # U80
+        ax.set_ylim([10**-8, 10**0.5])        
+    else:
+        # U60
+        ax.set_ylim([10**-18, 10**1])   
 
     ax.set_yscale("log")
     ax.tick_params(axis='both', which='major',labelsize=12)
-    #labels = ['Conv-CarryIn','Conv-Inflation'] # 2 sets
-    #for tick in ax.xaxis.get_major_ticks():
-        #tick.label.set_fontsize(14)
+    labels = ['Conv-CarryIn','Conv-Inflation'] # 2 sets
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(14)
 
-    ax.yaxis.set_ticklabels([])
-    #ax.boxplot(bxinput_2, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6))
-    # Figure 8
-    ax.boxplot(bxinput_2, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6, 0.6, 0.6, 0.6))
+    ax.yaxis.set_ticklabels([])    
+    ax.boxplot(bxinput_2, 0, '', labels=labels, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops, widths=(0.6, 0.6))
     ax.grid()
 
     #plt.legend(handles=[av, box, whisk], fontsize=12, frameon=True, loc=3)
@@ -218,7 +164,7 @@ def main():
             for res_ori, res_carry, res_inflation, res_conv_ori, res_conv_carry, res_conv_inflation in zip(results_ori, results_carry, results_inflation, results_conv_ori, results_conv_carry, results_conv_inflation):            
                 dataset.append([res_conv_ori, res_conv_carry, res_conv_inflation, res_ori['ErrProb'], res_carry['ErrProb'], res_inflation['ErrProb']])
             plot = plot_datasets(dataset, utilization)
-            save_pdf = PdfPages('./'+ ident + '_' + str(num_tasks) + '_' + str(num_sets)+  '_' +str(utilization)+ '_'+str(fault_rate)+'.pdf')
+            save_pdf = PdfPages('./outputs/Fig_6u'+str(utilization)+'.pdf')
             save_pdf.savefig(plot, bbox_inches='tight', pad_inches=0.0)
             save_pdf.close()
 
